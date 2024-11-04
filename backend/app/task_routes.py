4953a897b1cf682eab409d4d2cac4e0615d8ca6d
yiming huang <yiminghuang@umass.edu>
2024-11-03 19:00:32 -0500

    add get task by id

--- backend/app/task_routes.py
+++ backend/app/task_routes.py
@@ -1,5 +1,7 @@
+from crypt import methods
+
 from flask import Blueprint, jsonify
-from .models import Task
+from .models import Task, Employee
 
 task_bp = Blueprint('task_bp', __name__)
 
@@ -9,3 +11,15 @@ def get_tasks():
     tasks = Task.query.all()
     tasks_list = [task.to_dict() for task in tasks]
     return jsonify(tasks_list)
+
+@task_bp.route('/tasks/employee/<int:employee_id>', methods=['GET'])
+def get_tasks_by_id(employee_id):
+    # Query Task table for tasks assigned to the given employee_id
+    tasks = Task.query.filter(Task.assignee_id == employee_id).all()
+
+    # Check if tasks are found
+    if tasks:
+        tasks_list = [task.to_dict() for task in tasks]
+        return jsonify(tasks_list)
+    else:
+        return jsonify({"message": "No tasks found for this employee"}), 404
