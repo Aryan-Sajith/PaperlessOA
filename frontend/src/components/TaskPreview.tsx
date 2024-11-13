b00c5b38928a9d59d633cff2d02385ad669883b5
Aryan Sajith <aryan.sajith04@gmail.com>
2024-11-13 18:38:42 -0500

    Added the TaskPreview component for how tasks will look on the list of tasks(without being expanded)

---
+++ frontend/src/components/TaskPreview.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type TaskPreviewProps = {
+    title: string;
+    due_date: string;
+    // Add more as needed
+    onClick: () => void;
+}
+
+export default function TaskCard({
+    title,
+    due_date,
+    onClick
+}: Readonly<TaskPreviewProps>) {
+    return (
+        <div
+            onClick={onClick}
+            className="flex justify-between items-center p-3 bg-gray-100 hover:bg-gray-200 cursor-pointer rounded-md"
+        >
+            <p className="text-md font-medium">{title}</p>
+            <span className="text-sm text-gray-500">{due_date}</span>
+        </div>
+    );
+}
+
