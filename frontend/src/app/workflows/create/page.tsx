4e5e9204c7425be3e837879c98bbcc6149b48937
yiming huang <yiminghuang@umass.edu>
2024-11-18 22:06:58 -0500

    add workflow display page

---
+++ frontend/src/app/workflows/create/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import "./workflow_card.css"
+import Link from "next/link";
+
+const CreateWorkflow = () => {
+    return(
+        <main className="content">
+            <header className="header">
+                <h1>Name</h1>
+            </header>
+            <div className="card-container">
+                <Link href="/workflows/onboarding" >
+                    <div className="card">Onboarding: Add new Employees</div>
+                </Link>
+                <Link href={"/workflows/promotion"}>
+                    <div className="card">Promotion: Promote to a higher position</div>
+                </Link>
+                <Link href={"/workflows/resignation"}>
+                    <div className="card">Resignation: Resign for the job</div>
+                </Link>
+                <Link href={"/workflows/absence"}>
+                    <div className="card">Absence: Leave for work</div>
+                </Link>
+            </div>
+        </main>
+    );
+}
+
+export default CreateWorkflow;
