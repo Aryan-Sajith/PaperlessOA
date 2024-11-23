81cd7b8a7c31450e4560756d1d9ff77d1c766ce1
yiming huang <yiminghuang@umass.edu>
2024-11-23 16:59:28 -0500

    successfully create onboarding page and implement a post request to the backend (#11)

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,15 @@
 from app import create_app
+from flask import request, Response
 
 app = create_app()
+
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        res = Response()
+        res.headers['X-Content-Type-Options'] = '*'
+        return res
+
 
 
 if __name__ == '__main__':
